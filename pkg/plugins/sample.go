3740465d5114f332d6830ead0cee0327588ff73f
SeasonPilot <pilot1@foxmail.com>
2023-01-19 16:01:58 +0800

    feat: add SampleArgs

--- pkg/plugins/sample.go
+++ pkg/plugins/sample.go
@@ -5,10 +5,19 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	frameworkruntime "k8s.io/kubernetes/pkg/scheduler/framework/runtime"
 	framework "k8s.io/kubernetes/pkg/scheduler/framework/v1alpha1"
 )
 
+type SampleArgs struct {
+	FavoriteColor  string `json:"favorite_color,omitempty"`
+	FavoriteNumber int    `json:"favorite_number,omitempty"`
+	ThanksTo       string `json:"thanks_to,omitempty"`
+}
+
 type Sample struct {
+	args    *SampleArgs
+	handler framework.FrameworkHandle
 }
 
 var _ framework.PreFilterPlugin = &Sample{}
@@ -41,5 +50,18 @@ func (s Sample) Filter(ctx context.Context, state *framework.CycleState, pod *co
 //type PluginFactory = func(configuration runtime.Object, f v1alpha1.FrameworkHandle) (v1alpha1.Plugin, error)
 
 func SampleFactory(configuration runtime.Object, f framework.FrameworkHandle) (framework.Plugin, error) {
-	return &Sample{}, nil
+	args := getSampleArgs(configuration)
+	return &Sample{
+		args:    args,
+		handler: f,
+	}, nil
+}
+
+func getSampleArgs(obj runtime.Object) *SampleArgs {
+	sa := &SampleArgs{}
+	err := frameworkruntime.DecodeInto(obj, sa)
+	if err != nil {
+		return nil
+	}
+	return sa
 }
